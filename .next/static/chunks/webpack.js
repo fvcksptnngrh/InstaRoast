47919d5998f05d539fc3d76bd408a47f66a62e09
fvcksptnngrh <adhiseptianngrh@gmail.com>
2025-06-22 00:44:42 +0700

    fix: use correct /profile2 api endpoint

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "31e9cd4eb8816cd9"; }
+/******/ 		__webpack_require__.h = function() { return "2ffaca34dc86fdaa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
